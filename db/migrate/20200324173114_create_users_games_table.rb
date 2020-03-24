5cf28ab365eeb2ebd1bca4861b91368631ef7ba1
lalogaribo <garibo.eduardo90@gmail.com>
2020-03-24 11:08:17 -0700

    Created tables

--- db/migrate/20200324173114_create_users_games_table.rb
+++ db/migrate/20200324173114_create_users_games_table.rb
@@ -1,4 +1,12 @@
 class CreateUsersGamesTable < ActiveRecord::Migration[5.0]
   def change
+    create_table :user_games do |t|
+      t.integer :user_id
+      t.integer :game_id
+      t.integer :points
+      t.integer :correct_answers
+      t.integer :total_answers
+      t.timestamps
+    end
   end
 end
--- db/migrate/20200324173125_create_questions_table.rb
+++ db/migrate/20200324173125_create_questions_table.rb
@@ -1,4 +1,10 @@
 class CreateQuestionsTable < ActiveRecord::Migration[5.0]
   def change
+    create_table :questions do |t|
+      t.string :question
+      t.string :difficulty
+      t.boolean :correct_answer
+      t.integer :category_id
+    end
   end
 end
